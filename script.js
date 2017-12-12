346437110e7da418d59b547dc50295de54a6a9cb
kahunakhan <34471708+kahunakhan@users.noreply.github.com>
2017-12-12 08:34:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ window.open(ref, 'targetWindow', 'toolbar=no,
 status=no, menubar=no, scrollbars=no, resizable=no, width=10, height=10');
 })
 );
+  
+  
+function openWin() {
+    myWindow = window.open('', '', 'width=200, height=100');    // Opens a new window
+    myWindow.document.write("<p>This is 'myWindow'</p>");       // Some text in the new window
+}
 
+function moveWin() {
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
 </script>
 
 
