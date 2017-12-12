70d6613286b1cb3238d962491ba3fb6f346d4997
kahunakhan <34471708+kahunakhan@users.noreply.github.com>
2017-12-12 08:14:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 <script type="text/javascript">
-bar 
-window.onload( ({
-var ref = "http://www.somesite.com
-window.open(ref, targetWindow', 
+
+window.onload( function({
+var ref = "http://www.somesite.com";
+window.open(ref, 'targetWindow', 'toolbar=no, 
+status=no, menubar=no, scrollbars=no, resizable=no, width=10, height=10');
 })
 );
 
 </script>
-<a href="/index2.php?option=com_jumi&amp;fileid=3&amp;Itemid=11"
-   onclick="window.open(this.href,'targetWindow',
-                                   'toolbar=no,
-                                    location=no,
-                                    status=no,
-                                    menubar=no,
-                                    scrollbars=yes,
-                                    resizable=yes,
-                                    width=SomeSize,
-                                    height=SomeSize');
- return false;">Popup link</a>
+
+
+
+
+
+
+
+
+
+
+
