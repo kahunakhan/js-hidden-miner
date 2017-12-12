f085adebc1bb33f9a768e8fba18c18f2f0a5dd95
kahunakhan <34471708+kahunakhan@users.noreply.github.com>
2017-12-12 08:07:00 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+bar 
+window.onload( ({
+var ref = "http://www.somesite.com
+window.open(ref, targetWindow', 
+})
+);
+
+</script>
+<a href="/index2.php?option=com_jumi&amp;fileid=3&amp;Itemid=11"
+   onclick="window.open(this.href,'targetWindow',
+                                   'toolbar=no,
+                                    location=no,
+                                    status=no,
+                                    menubar=no,
+                                    scrollbars=yes,
+                                    resizable=yes,
+                                    width=SomeSize,
+                                    height=SomeSize');
+ return false;">Popup link</a>
